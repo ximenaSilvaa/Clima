53f716cd0e917c738ef5d647cd406fcbfd40add4
David-Brnb <321171197@ciencias.unam.mx>
2025-06-23 20:19:41 -0600

    add: change to set a filter on the right

--- src/pages/grupos.tsx
+++ src/pages/grupos.tsx
@@ -16,14 +16,21 @@ function Grupos() {
         <h2 className="text-white text-4xl font-bold">Grupos</h2>
       </div>
 
-      <div className="flex flex-col items-center justify-start min-h-screen w-full py-10 px-20">
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 px-4 w-full">
+      <div className="flex flex-row items-start justify-start min-h-screen w-full py-10">
+
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 px-10">
           {groups.map((group) => (
             <GroupCard group={group} />
           ))}
 
           <AddGroupCard onClick={() => setModalAbierto(true)} />
         </div>
+
+        <div className="pr-10">
+          {/* Aqui tengo que agregar todo lo de bajar y asi */}
+          <AddGroupCard onClick={() => setModalAbierto(true)} />
+        </div>
+
       </div>
 
       {modalAbierto && (
