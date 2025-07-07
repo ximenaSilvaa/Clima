b8f0f06355960d33ded0b3a635642392f3ec5f5c
David-Brnb <321171197@ciencias.unam.mx>
2025-07-07 12:56:17 -0600

    add: prepared the ui for students filters

--- src/pages/grupo.tsx
+++ src/pages/grupo.tsx
@@ -5,24 +5,30 @@ import Navbar from '../componentes/Navbar';
 import AddStudentCard from '../componentes/addStudentCard';
 import { useState } from 'react';
 import ModalAddStudent from '../componentes/modalAddStudent';
+import GroupFilter from '../componentes/groupFilter';
+import { Shift } from '../types/types';
 
 function Grupo() {
     const location = useLocation();
     const { students } = location.state || {};
     const [modalAbierto, setModalAbierto] = useState(false)
+    const [currentShift, setCurrentShift] = useState<Shift | "">("");
+    const [nameIncluded, setNameIncluded] = useState("");
+    const [teacherIncluded, setTeacherIncluded] = useState("");
 
     return (
         <div className="h-auto w-auto bg-primary flex flex-col">
             {/* header */}
             <Navbar/>
 
-            <div className='text-center mt-20 mb-20'>
+            <div className="text-center mt-20 mb-20">
                 <h2 className="text-white text-4xl font-semibold">Te encuentras en el grupo A </h2>
             </div>
 
             {/* contenido centrado */}
-            <div className="flex flex-col items-center justify-start min-h-screen w-full py-10 px-20">
-                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-20 px-4 w-full">
+            <div className="flex flex-row items-start justify-start min-h-screen w-full py-10 px-20">
+                
+                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-20 px-4">
                     {
                     students.map((student: StudentInformation) => (
                         <StudentCard student={student}/>
@@ -30,8 +36,18 @@ function Grupo() {
                     }
 
                     <AddStudentCard onClick={() => setModalAbierto(true)}/>
+                </div>
 
-
+                <div className="pl-20">
+                    <GroupFilter
+                        name={nameIncluded}
+                        teacher={teacherIncluded}
+                        shift={currentShift}
+                        onNameChange={setNameIncluded}
+                        onTeacherChange={setTeacherIncluded}
+                        onShiftChange={setCurrentShift}
+                        onClear={()=>{}}
+                    />
                 </div>
             </div>
 
